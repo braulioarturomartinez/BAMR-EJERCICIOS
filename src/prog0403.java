2bfd1f9fcd1f8c3729372aec0b1046b7a2e20f3e
braulioarturomartinez <braulioarturomartinez@gmail.com>
2026-04-21 23:56:27 +0000

    agg linea y prog0403

---
+++ src/prog0403.java
@@ -0,0 +1,18 @@
+//program0403.java
+// clase principal del programa que se utilizara
+// para probar las clases linea y hora
+public class prog0403 {
+    
+    public static void main (String[] args) {
+
+
+
+
+ // prueba de la clase linea
+ Relacionable <linea> linea1 = new Relacionable <linea> (new linea(), new linea());
+ Relacionable <linea> linea2 = new Relacionable <linea> (new linea(), new linea());
+ //comparacion de las lineas
+ System.out.println("Comparacion de las lineas: ");
+
+    }
+}
